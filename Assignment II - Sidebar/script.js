04cbe6d77812a4599d77ce3bd2561762594dbd37
Karthik Kasturi <karthikkasturi97@gmail.com>
2019-11-28 23:10:51 +0530

    sidebar done fr

--- Assignment II - Sidebar/script.js
+++ Assignment II - Sidebar/script.js
@@ -2,10 +2,11 @@ $(document).on('click', '.sidebar-mini .mini-item', function(){
     var allContainers = $('.sidebar-mini  .mini-item-container');
     allContainers.removeClass('active');
     $(this).parent().addClass('active');
-    // var that = this;
-    // allContainers.each(function(){
-    //     if($(this).has(that).length) {
-    //         $(this).addClass('active');
-    //     }
-    // });
+});
+
+
+$(document).on('click', '.sidebar .menu-item', function(){
+    var allContainers = $('.sidebar  .menu-item');
+    allContainers.removeClass('active');
+    $(this).addClass('active');
 });
